bbb41a12a84cec440996e064a16dd37210799153
rubhani <rubhani85@gmail.com>
2022-09-13 18:35:11 -0500

    File Upload Database

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,14 @@
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+)
+
+FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 150,
+    imageResizeTargetHeight: 100
+
+})
+
+FilePond.parse(document.body);
